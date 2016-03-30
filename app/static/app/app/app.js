fe6869a0008ac89d1752e0f458dccda262fba17b
Flávio Spinelli <flavio@dizain.com.br>
2016-03-30 17:47:03 -0300

    .

--- app/static/app/app/app.js
+++ app/static/app/app/app.js
@@ -1,28 +1,28 @@
 var app = angular.module('app',['ngRoute']);
  
-app.config(function($routeProvider)
+app.config(function($routeProvider, $locationProvider, $interpolateProvider)
 {
+   $interpolateProvider.startSymbol('{$');
+   $interpolateProvider.endSymbol('$}');
+
+   $locationProvider.html5Mode(true);
  
    $routeProvider
  
-   // para a rota '/', carregaremos o template home.html e o controller 'HomeCtrl'
    .when('/', {
       templateUrl : '/static/app/app/views/home.html',
       controller     : 'HomeCtrl',
    })
  
-   // para a rota '/sobre', carregaremos o template sobre.html e o controller 'SobreCtrl'
    .when('/sobre', {
       templateUrl : '/static/app/app/views/sobre.html',
       controller  : 'SobreCtrl',
    })
  
-   // para a rota '/contato', carregaremos o template contato.html e o controller 'ContatoCtrl'
    .when('/contato', {
       templateUrl : '/static/app/app/views/contato.html',
       controller  : 'ContatoCtrl',
    })
  
-   // caso não seja nenhum desses, redirecione para a rota '/'
-   .otherwise ({ redirectTo: '/' });
+   .otherwise ({ templateUrl:'/static/app/app/views/layout/404.html' });
 });
--- curso/urls.py
+++ curso/urls.py
@@ -11,8 +11,8 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
-	url(r'^$', include('app.urls')),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
+    url(r'^.*$', include('app.urls')),
 ]
