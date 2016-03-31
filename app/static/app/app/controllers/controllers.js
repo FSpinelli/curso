d9e432c74227bed9829b715ba693e612658f3e00
Flávio Spinelli <flavio@dizain.com.br>
2016-03-31 13:42:38 -0300

    .

--- app/static/app/app/controllers/controllers.js
+++ app/static/app/app/controllers/controllers.js
@@ -1,6 +1,20 @@
-app.controller('HomeCtrl', function($rootScope, $location)
+app.controller('HomeCtrl', function($rootScope, $location, $scope)
 {
-   $rootScope.activetab = $location.path();
+	// carousel
+	$scope.myInterval = 3000;
+   	$scope.slides = [
+      	{
+         	image: 'http://www.lfg.com.br/media/banner/concurso_1.jpg'
+      	},
+      	{
+         	image: 'http://www.lfg.com.br/media/banner/concurso_2.jpg'
+      	},
+      	{
+         	image: 'http://www.lfg.com.br/media/banner/concurso_3.jpg'
+      	}
+	];
+
+   	$rootScope.activetab = $location.path();
 });
  
 app.controller('SobreCtrl', function($rootScope, $location)
